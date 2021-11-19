371de941419b020d53c9b477fed14d1a0062fb76
Albertma126 <albert.ma.252@gmail.com>
2021-11-18 23:43:58 -0600

    Update imageManipulation.py

--- imageManipulation.py
+++ imageManipulation.py
@@ -10,13 +10,17 @@ import cv2
 import numpy as np
 
 sampleImg = cv2.imread('Resources\lena.png')
-# kernel = np.ones()
+kernel = np.ones((5,5), np.uint8)   # to be used for dilation
 
-imgColorConvert = cv2.cvtColor(sampleImg,cv2.COLOR_BGR2GRAY)
-imgBlur = cv2.GaussianBlur(sampleImg, (7,7), 0)
-imgCanny = cv2.Canny(sampleImg, 150, 200)
+imgColorConvert = cv2.cvtColor(sampleImg,cv2.COLOR_BGR2GRAY)    # Color Conversion
+imgBlur = cv2.GaussianBlur(sampleImg, (7,7), 0) # De-noising the Image by blurring it
+imgCanny = cv2.Canny(sampleImg, 150, 200)   # Edge Detection
+imgDilation = cv2.dilate(imgCanny, kernel, iterations=1)    # Increases thickness of edges from canny using iterations
+imgEroded = cv2.erode(imgDilation, kernel, iterations=1)    # Essentially the opposite of dilation
 
 cv2.imshow("Gray Image", imgColorConvert)
 cv2.imshow("Blurred Image", imgBlur)
 cv2.imshow("Image Canny", imgCanny)
+cv2.imshow("Image Dilation", imgDilation)
+cv2.imshow("Image Erosion", imgEroded)
 cv2.waitKey(0)
